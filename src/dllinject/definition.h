41d9fadb27d7fc008c0e79f2e924fefb30d2c9fb
Alexey <38497966+AlexeyGalckin@users.noreply.github.com>
2020-11-22 11:23:37 +0300

    Update definition.h

--- src/dllinject/definition.h
+++ src/dllinject/definition.h
@@ -13,4 +13,7 @@ public:
 	std::basic_string<char, struct std::char_traits<char>, class std::allocator<char> > const& PixelEngine::containerVersion(class std::basic_string<char, struct std::char_traits<char>, class std::allocator<char> > const& ) const;
 	// or simply:
 	//const std::string& PixelEngine::containerVersion(const std::string&) const;
+private:
+	static constexpr size_t SIZE = 64; // Make sure we have enough space to hold the class
+	char _data[SIZE];
 };
